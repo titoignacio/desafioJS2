14304453908ff7ed8e9973ab82808ee0fb072c66
titoignacio <h.villalobosm03@gmail.com>
2019-11-07 20:10:50 -0300

    desafiojvs2

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+// desafio 1
+var text = "Hola desafio!! Qué facil es incluir 'comillas simples' y \"comillas dobles\" en este mensaje!";
+alert(text);
+
+// desafio 2
+var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+for ( i = 0; i < meses.length; i++){
+    console.log(meses[i])
+     }
